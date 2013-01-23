b89bdc32f336ab5b8a344bf05664c68c925b8e09
student <student@student-VirtualBox>
2013-01-23 01:33:07 +0100

    keep going

--- s2/src/main/java/servis/worker/WorkerData.java
+++ s2/src/main/java/servis/worker/WorkerData.java
@@ -69,26 +69,4 @@ public class WorkerData {
 		this.address = address;
 	}
 	
-	/// PO CO ??? 
-	/*
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		WorkerData other = (WorkerData) obj;
-		if (id!= other.id)
-			return false;
-		return true;
-	}*/
 }
